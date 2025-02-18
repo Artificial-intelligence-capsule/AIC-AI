106c2c9e20285eab91f316bab321422209ba3514
AIC-AI <sanlove104341@gmail.com>
2025-02-18 22:38:25 +0800

    嘿嘿嘿

--- about/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    let header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.style.background = "rgba(0, 0, 0, 0.8)";
-    } else {
-        header.style.background = "rgba(0, 0, 0, 0.5)";
-    }
-});
